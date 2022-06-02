998e432e917464f2ef63ccdaebd0df111d056492
SivaranjiniSanthanakrishnan <43466009+SivaranjiniSanthanakrishnan@users.noreply.github.com>
2022-06-02 12:02:56 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -45,3 +45,73 @@ function addrow(){
     td2.innerText = "28";
 }
 
+function deleterow(){
+    document.getElementById('table').deleteRow(0)
+}
+
+function eventTriggered(evtValue){
+    console.log(`Triggered event is ${evtValue.value}`)
+}
+
+function eventButton(){
+    console.log(`Button is clicked`)
+}
+
+function addCountry(){
+    // Select the select box (target element to add)
+    let selectElement = document.getElementById('country');
+
+    // Create option element
+    let newOption= document.createElement('option');
+    newOption.innerText = document.getElementById('country-value').value;
+    newOption.setAttribute('value', document.getElementById('country-value').value)
+
+    selectElement.append(newOption);
+    document.getElementById('country-value').value = "";
+}
+
+function removeCountry(){
+    let selectElement = document.getElementById('country');
+    selectElement.remove(selectElement.selectedIndex);
+}
+
+
+document.getElementsByTagName('ul')[0].setAttribute('id', 'unordered-list');
+let ul = document.getElementById('unordered-list');
+let li = document.createElement('li');
+li.innerHTML = 'Contact Us';
+ul.append(li);
+
+// Retrieve First and Last Child
+let firstElement = ul.firstElementChild.innerText;
+let lastElement = ul.lastElementChild.innerText;
+document.getElementById('appendData').innerHTML += (`First and Last ele is <b> ${firstElement} and ${lastElement} </b>`)
+
+
+// QuerySelector
+let divElementQuerySelector = document.querySelectorAll('.para');
+console.log(divElementQuerySelector);
+
+
+// setTimeout
+setTimeout(() => {
+    console.log('Executed after 3 seconds')
+}, 3000);
+
+//cleatTimeout
+const timeout = () => {console.log("Executed after 5 seconds")}
+const timer = setTimeout(timeout, 5000)
+
+setTimeout(() => {
+    clearTimeout(timer)
+}, 7000);
+
+// setInterval
+const intervalTimer = setInterval(() => {
+    console.log('Executed after 1 second')
+}, 1000)
+
+// clearInterval
+setTimeout(() => {
+    clearInterval(intervalTimer)
+}, 5000);
---
+++ recap.js
@@ -0,0 +1,6 @@
+const imgTag = document.querySelector('img');
+imgTag.setAttribute('src', './nature.jpeg');
+imgTag.setAttribute('height', '300');
+imgTag.setAttribute('width', '300');
+
+
